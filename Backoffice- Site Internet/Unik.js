6f8ec74bc44c8a957e44a376cbcd0189e841d902
Carole-LL <caroleviva@gmail.com>
2020-01-15 16:33:20 +0100

    js boucle

--- Backoffice- Site Internet/Unik.js
+++ Backoffice- Site Internet/Unik.js
@@ -1,8 +1,12 @@
-//API adresse web :
+//API- adresse web :
 //https://rapidapi.com/apidojo/api/asos2
 
+
+
+//Obtenir donnée du lien API
 async function reecrireAppels() {
-    try {
+    //try {
+
       let blabla = await fetch("https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-radius?radius=50&lng=-157.895277&lat=21.265600", {
         "method": "GET",
         "headers": {
@@ -10,11 +14,37 @@ async function reecrireAppels() {
             "x-rapidapi-key": "b5a32561b6msh36a70cb15ae375cp16778ejsn795ffec91187"
         }
     });
+
       blabla = await blabla.json();
-      console.log(blabla);
-    } catch (e) {
-      console.log(e);
+      let tableau = blabla;
+      await console.log(tableau);
+    
+      //} catch (e) {
+      //console.log(e);
+    
+    var test2 = document.getElementsByClassName("graph");
+    var test = document.getElementById('graph'); //selection div pour affichage donnée
+    function remplir(){
+      for(var i = 0; i< test2.length; i++){
+        if(i < 6){
+          test2[i].innerHTML = tableau[i].name;
+        }
+        if(i>5){
+          test2[i].innerHTML = tableau[i-4].city;
+        }
+      }
+   
+      
     }
-}
+
+    remplir();
+    console.log(test2);
+    
+
+  }
 
 reecrireAppels();
+
+
+//créer 3 div dans une div et leurs mettre des classes différentes (nom, ville et info)
+//
