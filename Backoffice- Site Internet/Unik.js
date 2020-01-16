4ccb8f5c13230b5e5fd9481b682a8138400565b4
Carole-LL <caroleviva@gmail.com>
2020-01-16 12:23:28 +0100

    users page

--- Backoffice- Site Internet/Unik.js
+++ Backoffice- Site Internet/Unik.js
@@ -7,13 +7,13 @@
 async function reecrireAppels() {
     //try {
 
-      let blabla = await fetch("https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/by-radius?radius=50&lng=-157.895277&lat=21.265600", {
+      let blabla = await fetch("https://dawn2k-random-german-profiles-and-names-generator-v1.p.rapidapi.com/?count=10&gender=b&maxage=40&minage=30&cc=all&email=gmail.com%252Cyahoo.com&pwlen=12&ip=a&phone=l%252Ct%252Co&uuid=false&lic=false&color=false&seed=helloworld&images=false&format=json", {
         "method": "GET",
         "headers": {
-            "x-rapidapi-host": "cometari-airportsfinder-v1.p.rapidapi.com",
-            "x-rapidapi-key": "b5a32561b6msh36a70cb15ae375cp16778ejsn795ffec91187"
+          "x-rapidapi-host": "dawn2k-random-german-profiles-and-names-generator-v1.p.rapidapi.com",
+          "x-rapidapi-key": "b5a32561b6msh36a70cb15ae375cp16778ejsn795ffec91187"
         }
-    });
+      });
 
       blabla = await blabla.json();
       let tableau = blabla;
@@ -22,24 +22,44 @@ async function reecrireAppels() {
       //} catch (e) {
       //console.log(e);
     
-    var test2 = document.getElementsByClassName("graph");
-    var test = document.getElementById('graph'); //selection div pour affichage donnée
-    function remplir(){
-      for(var i = 0; i< test2.length; i++){
-        if(i < 6){
-          test2[i].innerHTML = tableau[i].name;
+      //var test = document.getElementById('graph'); //selection div pour affichage donnée
+    var noms = document.getElementsByClassName("names");
+    var ville = document.getElementsByClassName("city");
+    var tel = document.getElementsByClassName("phone");
+
+    function remplirNoms(){
+      for(var i = 0; i< noms.length; i++){
+        if(i < 8){
+          noms[i].innerHTML = tableau[i].firstname+" "+tableau[i].location.city;
         }
-        if(i>5){
-          test2[i].innerHTML = tableau[i-4].city;
+        // if(i>5){
+        //   name[i].innerHTML = tableau[i].city;
+        // }
+      }
+    }
+    remplirNoms();
+
+    function remplirVille(){
+      for(var i = 0; i< ville.length; i++){
+        if(i < 8){
+          ville[i].innerHTML = tableau[i].location.city;
         }
       }
-   
-      
+
+    }
+    remplirVille();
+
+    function remplirTel(){
+      for(var i = 0; i< tel.length; i++){
+        if(i < 8){
+          tel[i].innerHTML = tableau[i].phone.mobile;
+        }
+      }
+
     }
 
-    remplir();
-    console.log(test2);
-    
+    remplirTel();
+
 
   }
 
